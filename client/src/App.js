e0e867232d32e4c7d4c726fcfd46650fd74b7ae5
Christian <Chrisaduenas@gmail.com>
2020-02-21 17:46:01 -0800

    useLocalStorage custom hook implemented, saves persons name on the localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
 import PlayerData from './components/PlayerData';
+import useLocalStorage from './components/useLocalStorage';
 
 function App() {
+  const [name, setName] = useLocalStorage('name', '')
+
+
   return (
     <div className="App">
+      <div>
+        <input 
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange= {e => setName(e.target.value)}
+          />
+      </div>
       <PlayerData />
     </div>
   );
